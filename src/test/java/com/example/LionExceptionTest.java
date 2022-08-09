36d4a0e523872b84cc21d886b32f7bbc2fc48a3d
Katerina Pipteva <ekaterina.pipteva@aliexpress.ru>
2022-08-10 00:02:05 +0300

    sprint 6. Second fix in LionExceptionTest class. There is some comments with the reason to leave messages to reviewer

--- src/test/java/com/example/LionExceptionTest.java
+++ src/test/java/com/example/LionExceptionTest.java
@@ -3,20 +3,35 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import static org.junit.Assert.assertEquals;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionExceptionTest {
     @Mock
     Feline feline;
 
-    @Test(expected = Exception.class)
+    //Комментарий для ревьюера:
+    //Не поняла почему старая реализация не подходила, у меня старый тест падает, если выставить
+    //коректные данные, например, "Самка" в параметр sex. И тест проходит, если выставить некорректный параметр
+
+    //Старый тест закомментировала (знаю что лучше его убрать совсем)
+   /* @Test(expected = Exception.class)
     public void lionConstructorGetException() throws Exception{
         try {
-            Lion lion = new Lion("Интерсекс", feline);
+            Lion lion = new Lion("Самка", feline);
         } catch (Exception e) {
             assertEquals("Используйте допустимые значения пола животного - самец или самка", e.getMessage());
             throw e;
         }
+    }*/
+
+    //но перепишу по вашему совету тест с assertTrows (с ним, конечно, тест стал более аккуратным)
+
+    @Test
+    public void lionConstructorGetException() {
+        Exception exception = assertThrows(Exception.class, () -> { Lion lion = new Lion("Интерсекс", feline);});
+        assertTrue(exception.getMessage().contains("Используйте допустимые значения пола животного - самец или самка"));
     }
 }
